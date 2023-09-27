b4824fd2558a00df851f02cc66fa749e81b692c9
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2023-09-27 09:26:42 -0700

    configuring your server with Puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+include stdlib
+
+exec { 'install nginx':
+  command  => 'sudo apt update && sudo apt-get -y nginx',
+  provider => shell,
+}
+
+file { '/etc/nginx/sites-available/default':
+  content => 'server {
+		listen 80 default_server;
+		server_name _;
+		root /var/www/html;
+		location / {
+		  index.html
+          	}
+		rewrite /redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+	}',
+  require => Exec['install nginx'],
+}
+
+file { '/ver/www/html/index.html':
+  ensure  =>  'file',
+  content =>  'Hello World!',
+  require =>  Exec['install nginx'],
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+  require  => File['/etc/nginx/sites-available/default'],
+}
